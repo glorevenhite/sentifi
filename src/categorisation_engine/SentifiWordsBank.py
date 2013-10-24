3c7f97ded227b5289fad4c7d46a019acb1526bbe
glorevenhite <glorevenhite@gmail.com>
2013-10-24 16:08:00 +0700

    SentifiWordsBank

--- src/categorisation_engine/SentifiWordsBank.py
+++ src/categorisation_engine/SentifiWordsBank.py
@@ -1,22 +1,9 @@
 import MySQLdb
 import pprint
 from Rule import Rule
+
+
 class SentifiWordsBank(object):
-    def __init__(self):
-        pass
-    @property
-    def wordsbank(self):
-        return self._wordsbank
-
-    @wordsbank.setter
-    def wordsbank(self, value):
-        self._wordsbank = value
-
-    def add_word(self, word):
-        self._wordsbank.append(word)
-
-    def get_compound_words(self):
-        pass
 
     def tokenizer(self, content, keywords):
 
@@ -38,13 +25,46 @@ class SentifiWordsBank(object):
             content = content.replace(tmp, cw)
         return content
 
-    def build_keywords(self, ruleset):
-        keywords = []
-        # Having ruleset must be applied to field. Some field_content don't have any rule to dectect PROFILE_TYPE, exp: fullname of Personal
-        if (ruleset != None):
-            for r in ruleset:
-                keywords = keywords + r.keywords
-        return keywords
+    def build_sorted_dictionary(self, list_words):
+
+        dict_word = {}
+        for phrase in list_words:
+            #spliting using space
+            splitted_words = phrase.split(" ")
+            words_count = len(splitted_words)
+
+            #replacing space by hyphen then lowercase
+            hyphen_phrase = phrase.replace(" ", "-").lower()
+            dict_word.update({hyphen_phrase: words_count})
+
+        sorted_dictionary = sorted(dict, key=lambda k: dict[k], reverse=True)
+
+        return sorted_dictionary
+
+    def hyphenize_compound_words_in(self, list_words):
+        #strip space in left and right
+        processing_content = self.content.strip()
+
+        #replace multi-space by sing space
+        processing_content = processing_content.replace("  ", " ")
+
+        #replace hyphen in compound-word by space
+        vocabulary = []
+        sorted_dictionary = SentifiWordsBank().build_sorted_dictionary(list_words)
+        for word in sorted_dictionary:
+            #change hyphen-words into normal
+            usual_word = word.replace('-', ' ')
+
+            #put that word into new vocabulary
+            vocabulary.append(usual_word)
+
+        #Replace 'compound words' in content by the one with hyphen, i.e. compound-words
+        for cw in sorted_dictionary:
+            #temporaly
+            tmp = cw.replace("-", " ")
+            processing_content = processing_content.replace(tmp, cw)
+
+        return processing_content
 
     def get_compound_word(self):
         compound_nount = []
