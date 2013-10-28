82af73ffdcb0897adde831eb6ea13a9fef209743
VINH VO <vinh.vo@sentifi.com>
2013-10-28 10:11:20 +0700

    enable scan multi fields in categorisation

--- src/categorisation_engine/Classifier.py
+++ src/categorisation_engine/Classifier.py
@@ -24,44 +24,39 @@ class Classifier(object):
             dict_result = {}
 
             description_field = SentifiField(profile.description)
-            field_id = 1
+            fullname_field = SentifiField(profile.fullname)
+            fields = {1: description_field, 3: fullname_field}
 
-            #for each phase,
-            for phase in PHASE_VALUES:
-                print phase
-                #build message to send server to get CATEGORIES in this phase
-                message = {'type': 'ruleset', 'phase': phase, 'field_id': field_id}
-                client = Client()
-                result = client.send(message)
-                ruleset = {}
-                try:
-                    ruleset = simplejson.loads(result)[phase]
-                except:
-                    pass
-                dict_r = dict(ruleset)
+            for field in fields:
+                field_id = field
+                field_content = fields.get(field_id)
 
-                for rule in dict_r:
-                    print rule
-                    print len(dict_r.get(rule))
-                    for dct_r in dict_r.get(rule):
-                        r = Rule()
-                        r.rule_set_name = rule
-                        r.inc_keywords = dict_r.get(rule).get(dct_r)['status']['and']
-                        r.exc_keywords = dict_r.get(rule).get(dct_r)['status']['not']
-                        r.display()
-                        if description_field.is_complied(r):
-                            print phase, r.rule_set_name
-                            profile.set_category(phase, r.rule_set_name)
+                #for each phase,
+                for phase in PHASE_VALUES:
+                    print phase
+                    #build message to send server to get CATEGORIES in this phase
+                    message = {'type': 'ruleset', 'phase': phase, 'field_id': field_id}
+                    client = Client()
+                    result = client.send(message)
+                    ruleset = {}
+                    try:
+                        ruleset = simplejson.loads(result)[phase]
+                    except:
+                        pass
+                    dict_r = dict(ruleset)
 
-                    #cr = ComplexRule(rule, dict_r.get(rule).keys()[0], dict_r.get(rule))
-                    #rules = cr.rules
-                    #print len(rules)
-                    #for r in rules:
-                    #    if phase == "PROFILE TYPE":
-                    #        r.display()
-                    #    if description_field.is_complied(r):
-                    #        print phase, r.rule_set_name
-                    #        profile.set_category(phase, r.rule_set_name)
+                    for rule in dict_r:
+                        print rule
+                        print len(dict_r.get(rule))
+                        for dct_r in dict_r.get(rule):
+                            r = Rule()
+                            r.rule_set_name = rule
+                            r.inc_keywords = dict_r.get(rule).get(dct_r)['status']['and']
+                            r.exc_keywords = dict_r.get(rule).get(dct_r)['status']['not']
+                            r.display()
+                            if field_content.is_complied(r):
+                                print phase, r.rule_set_name
+                                profile.set_category(phase, r.rule_set_name)
 
         return list_profiles
 #
@@ -70,7 +65,7 @@ class Classifier(object):
 
 json = {'screen_name': 'glorevenhite', 'description': 'I am a financial analyst', 'name': 'Vo Truong Vinh'}
 p1 = TwitterProfile(json)
-p1.description = "I am financial analyst me"
+p1.description = "I am a financial analyst me"
 p1.screen_name = "glorevenhite"
 
 
