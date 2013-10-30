ca1e02c89c3a4f22479d434b6888314abd048e67
VINH VO <vinh.vo@sentifi.com>
2013-10-30 15:41:15 +0700

    Constants

--- src/crawler/Constant.py
+++ src/crawler/Constant.py
@@ -1,7 +1,16 @@
 __author__ = 'vinh.vo@sentifi.com'
 
-PATH = "D:\\SENTIFI_DATA\\mention\\"
-OUTPUT_PATH = PATH + "output\\processing\\"
+HOME_PATH = "D:\\SENTIFI_DATA\\"
+
+MENTION_PATH = HOME_PATH + "mention\\"
+MENTION_INPUT = MENTION_PATH + "input\\"
+MENTION_OUTPUT_PATH = MENTION_PATH + "output\\processing\\"
+
+MENTION_HEADER = ['twitter_screen_name_user', 'tweet_id', 'tweet_text', 'tweet_create_time', 'twitter_id',
+                  'twitter_screen_name', 'twitter_full_name', 'twitter_address', 'twitter_email',
+                  'twitter_website_url', 'twitter_website_full_url', 'twitter_description', 'twitter_image',
+                  'twitter_image_thumbnail', 'twitter_followers_count', 'twitter_followings_count',
+                  'twitter_statuses_count', 'twitter_listed_count']
 
 
 
