cfde3d6a5a9057a96d09ad3adc8bed5eac557149
VINH VO <vinh.vo@sentifi.com>
2013-10-30 15:05:06 +0700

    categorisation

--- src/categorisation_engine/Categorizer.py
+++ src/categorisation_engine/Categorizer.py
@@ -6,7 +6,7 @@ from TwitterProfile import TwitterProfile
 from CategorisationMatrix import CategorisationMatrix
 from RuleSet import RuleSet
 from Rule import Rule
-
+import pprint
 
 class Categorizer(object):
     def categorize_twitter_profile(self, list_profiles):
@@ -22,11 +22,12 @@ class Categorizer(object):
 
             # Put above fields into a list
             fields = {TWITTER_FULL_NAME: field_full_name, TWITTER_SCREEN_NAME: field_screen_name, TWITTER_DESCRIPTION: field_description}
-
+            print fields
             # For each phase of categorisation process, i.e. Profile Type, Publisher Group, Category 1, Category 2
             #
             for stage in PHASE_VALUES:
                 #creating matrix of categorisation result
+
                 matrix = self._create_categorisation_result_matrix(fields.keys(), stage)
 
                 # For each field, get all applicable rules.
@@ -38,11 +39,15 @@ class Categorizer(object):
 
                     if list_rule_subset is not None:
                         for subset in list_rule_subset:
-                            score = field._apply_rule_subset(subset)
-                            matrix.increase_by(field_id,)
+                            #print subset.cat_name
+                            score = field.apply_rule_subset(subset)
+                            #print score
+                            matrix.increase_by(field_id, subset.cat_name, score)
 
-                    print list_rule_subset
+                matrix.display()
     def _get_rule_subset_by_phase_and_field(self, stage_name, field_id):
+        list_rulesets = []
+
         message = {'type': 'subset', 'phase': stage_name, 'field_id': field_id}
         client = Client()
         print field_id, stage_name
@@ -52,35 +57,42 @@ class Categorizer(object):
         #    subset_id:{'exclusion':[m1,m2,m3], 'rules':{r
         # id:[x1,y1], rid:[x1,y2}}}
         result = dict(client.send(message))
-        print result
+
         if result.get('status') == SERVER_STATUS_OK and result.get('data') != {}:
 
             dict_rule_subsets = result.get('data')
-            print dict_rule_subsets
-            cat_name = dict_rule_subsets.keys()[0]
-            print cat_name
-            for subset in dict_rule_subsets.get(cat_name):
-                ss = RuleSet()
-                ss.cat_name = cat_name
-                ss.exclusion = subset.get('inclusion')
-                rules = subset.get('rules')
-                for rule in rules:
-                    r = Rule()
-                    r.inclusion = rule
-        return None
+            #pprint.pprint(dict_rule_subsets)
+
+            for cat_name in dict_rule_subsets.keys():
+                subsets = dict_rule_subsets.get(cat_name)
+
+                for subset in subsets:
+                    ss = RuleSet()
+                    ss.cat_name = cat_name
+                    ss.exclusion = subsets.get(subset).get('exclusion')
+                    rules = subsets.get(subset).get('rules').values()
+
+                    for rule in rules:
+                        r = Rule()
+                        r.inc_keywords = rule
+                        ss.rules.append(rule)
+                    list_rulesets.append(ss)
+
+        return list_rulesets
 
     def _create_categorisation_result_matrix(self, field_names, phase):
         #get all classes to which a profile will be assigned in given phase
 
         list_class_names = self._get_classes_by_phase_name(phase)
         list_field_names = field_names
+
         matrix = CategorisationMatrix(list_field_names, list_class_names)
 
         return matrix
     def _get_rules_by_phase_and_field(self, stage_name, field_id):
         message = {'type': 'ruleset', 'phase': stage_name, 'field_id': field_id}
         client = Client()
-        print message
+
         result = dict(client.send(message))
 
         return result
@@ -99,7 +111,7 @@ class Categorizer(object):
 json = {'screen_name': 'glorevenhite', 'description': 'I am a financial analyst', 'name': 'Vo Truong Vinh'}
 p = TwitterProfile(json)
 p.screen_name = "glorevenhite"
-p.description = "I am a financial analyst"
+p.description = "I am a financial analyst sell side journalist news"
 p.fullname = 'Vo Truong Vinh'
 
 list_profile = [p]
