e6d2deb194c96e196040f7f2e51208e357e672ea
VINH VO <vinh.vo@sentifi.com>
2013-11-04 17:38:04 +0700

    IOUtils

--- src/crawler/utils/IOUtils.py
+++ src/crawler/utils/IOUtils.py
@@ -1,18 +1,15 @@
-import os
 import csv
-import json
 import io
-import pprint
-
-from os import path
 from PathUtils import PathUtils
+
 
 class IOUtils(object):
 
-    def read_list_from_csv(self, file_path):
+    @staticmethod
+    def read_list_from_csv(file_path):
         results = []
 
-        file = open(file_path, "rU")
+        f = open(file_path, "rU")
 
         try:
             reader = csv.reader(file, delimiter=",", dialect='excel')
@@ -21,16 +18,16 @@ class IOUtils(object):
         except IOError:
             print IOError
 
-
-        file.close()
+        f.close()
 
         return results
 
     #Data is the first column in csv file
-    def read_first_column_in_csv(self, file_path):
+    @staticmethod
+    def read_first_column_in_csv(file_path):
         results = []
 
-        file = open(file_path, "rU")
+        f = open(file_path, "rU")
 
         try:
             reader = csv.reader(file, delimiter=",", dialect='excel')
@@ -39,19 +36,21 @@ class IOUtils(object):
         except IOError:
             print IOError
 
-        file.close()
+        f.close()
 
         return results
 
-    def save_list_to_csv(self, header, list_contents, file_path):
+    @staticmethod
+    def save_list_to_csv(header, list_contents, file_path):
         #Create file_path if the file doesn't exist
         PathUtils().create_path_if_not_exists(file_path)
 
-        file = open(file_path, 'ab')    #Appending file
+        #Appending file
+        f = open(file_path, 'ab')
         wr = csv.writer(file, dialect='excel')
 
         #write down the header first
-        if (header != None):
+        if header is not None:
             wr.writerow(header)
 
         for row in list_contents:
@@ -60,28 +59,31 @@ class IOUtils(object):
             except IOError:
                 print IOError
 
-        file.close()
+        f.close()
 
-    def save_json_data_to_file(self, json_data, file_name, path):
-        file = io.open(path + file_name, 'w', encoding='utf-8')
+    @staticmethod
+    def save_json_data_to_file(json_data, file_name, path):
+        f = io.open(path + file_name, 'w', encoding='utf-8')
 
-        file.write(unicode(json_data))
+        f.write(unicode(json_data))
 
-        file.close()
+        f.close()
 
-    def save_objects_to_csv(self, objects, file_path):
+    @staticmethod
+    def save_objects_to_csv(objects, file_path):
         pass
 
     #Combine all files in same directory into a master files
+    @staticmethod
     def combine_csv_file_in_same_dir(self, path, has_header):
         results = []
 
-        filenames = PathUtils().get_list_filename(path)
+        file_names = PathUtils().get_list_filename(path)
 
-        for file in filenames:
+        for file_name in file_names:
             contents = []
-            if(has_header):
-                contents = self.read_list_from_csv(path + file)
+            if has_header:
+                contents = self.read_list_from_csv(path + file_name)
                 #remove header
                 del contents[0]
                 print len(contents)
@@ -91,13 +93,14 @@ class IOUtils(object):
         results = self.remove_duplication_list(results)
         return results
 
-    def remove_duplication_list(self, list):
-        dict = {}
-        for item in list:
-            dict.update({item[0]:item})
+    @staticmethod
+    def remove_duplication_list(list_items):
+        dict_result = {}
+        for item in list_items:
+            dict_result.update({item[0]: item})
 
-        len(dict.values())
-        return dict.values()
+        len(dict_result.values())
+        return dict_result.values()
 
 #PATH = "D:\\online-cloud\\Dropbox\\Sentifi Analytics\\5. Dan\\Treasury Bill\\"
 #has_header = True
